dd4c93488f4917486f63ab2402268bc99a8075ff
theo-commits <me@tlindsey.cloud>
2022-01-10 14:34:57 -0600

    updates website message

--- lenslocked.com/main.go
+++ lenslocked.com/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to the Website!</h1>")
+	fmt.Fprint(w, "<h1>Welcome to my Website!</h1>")
 }
 
 func main() {
