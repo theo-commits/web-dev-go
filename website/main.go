b0e23e068394a0b2a87db3dd6c939c9c5d9284d3
theo-commits <me@tlindsey.cloud>
2022-01-10 20:02:28 -0600

    Updated error page

--- website/main.go
+++ website/main.go
@@ -23,10 +23,17 @@ func faq(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>SO you have some questions?</h1> <p>You should probably just google them tbh :/</p>")
 }
 
+func error404(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1> Hm, that's not right... </h1> <p> Penguin_404</p>")
+}
+
 func main() {
+	var e http.Handler = http.HandlerFunc(error404)
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/faq", faq)
+	r.NotFoundHandler = e
 	http.ListenAndServe(":3000", r)
 }
