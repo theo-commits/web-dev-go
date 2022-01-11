d357028135a636c746a88fe1de3c687c21b57a1f
theo-commits <me@tlindsey.cloud>
2022-01-10 19:42:45 -0600

    Added a FAQ page

--- website/main.go
+++ website/main.go
@@ -18,9 +18,15 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "To get into contact with me, please send an email to <a href=\"mailto:me@tlindsey.cloud\"> me@tlindsey.cloud</a>.")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>SO you have some questions?</h1> <p>You should probably just google them tbh :/</p>")
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
 	http.ListenAndServe(":3000", r)
 }
