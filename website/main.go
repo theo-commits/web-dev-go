016bd75a792d4aa939255a9b4b1e6a5b0e9e382e
theo-commits <me@tlindsey.cloud>
2022-01-14 22:35:40 -0600

    added some comments

--- website/main.go
+++ website/main.go
@@ -7,22 +7,26 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// Home page for website, uses Header.().Set() func to read html tags
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1> Welcome to my Website! </h1>")
 
 }
 
+// contact page for website
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "To get into contact with me, please send an email to <a href=\"mailto:me@tlindsey.cloud\"> me@tlindsey.cloud</a>.")
 }
 
+// Create FAQ page
 func faq(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>SO you have some questions?</h1> <p>You should probably just google them tbh :/</p>")
 }
 
+// Error handling for none exisitant pages. TODO: Add background image
 func error404(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1> Hm, that's not right... </h1> <p> Penguin_404</p>")
@@ -34,6 +38,6 @@ func main() {
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/faq", faq)
-	r.NotFoundHandler = e
+	r.NotFoundHandler = e // replace defautl 404 response with custom response
 	http.ListenAndServe(":3000", r)
 }
